9c7b549b7d642f320e4d3a11976290be18b8b48c
winton <mail@wintoni.us>
2010-01-28 14:31:15 -0800

    More tasks tweaks

--- lib/require/tasks.rb
+++ lib/require/tasks.rb
@@ -1,3 +1,39 @@
+class Require
+  module Tasks
+    
+    def self.set(*types)
+      if types.include?(:ask_docs)
+        $stdout.puts "Generate documentation?"
+        @@gen_docs = $stdin.gets.downcase[0..0] == 'y'
+      elsif types.include?(:docs)
+        @@gen_docs = true
+      elsif types.include?(:no_docs)
+        @@gen_docs = false
+      end
+      if types.include?(:ask_sudo)
+        $stdout.puts "Use sudo?"
+        @@use_sudo = $stdin.gets.downcase[0..0] == 'y'
+      elsif types.include?(:sudo)
+        @@gen_docs = true
+      elsif types.include?(:no_sudo)
+        @@use_sudo = false
+      end
+    end
+    
+    def self.rm_pkg
+      cmd = "rm -Rf pkg"
+      $stdout.puts cmd
+      system cmd
+    end
+    
+    def self.run(cmd)
+      cmd = "#{'sudo ' if @@use_sudo}#{cmd}#{' --no-ri --no-rdoc' unless @@gen_docs}"
+      $stdout.puts cmd
+      system cmd
+    end
+  end
+end
+
 desc "Package gem"
 Rake::GemPackageTask.new(Require.instance) do |pkg|
   pkg.gem_spec = Require.instance
@@ -6,10 +42,21 @@ end
 namespace :gem do
   desc "Install gem"
   task :install do
-    Rake::Task['clobber_package'].invoke
+    Require::Tasks.rm_pkg
     Rake::Task['gem'].invoke
-    `sudo gem uninstall #{Require.name} -x`
-    `sudo gem install pkg/#{Require.name}*.gem`
+    Require::Tasks.set :docs, :ask_sudo
+    Require::Tasks.run "gem uninstall #{Require.name} -x"
+    Require::Tasks.run "gem install pkg/#{Require.name}*.gem"
+    Require::Tasks.rm_pkg
+  end
+  
+  desc "Push gem"
+  task :push do
+    Require::Tasks.rm_pkg
+    Rake::Task['gem'].invoke
+    Require::Tasks.set :docs, :no_sudo
+    Require::Tasks.run "gem push pkg/#{Require.name}*.gem"
+    Require::Tasks.rm_pkg
   end
 end
 
@@ -23,15 +70,9 @@ end
 namespace :gems do
   desc "Install gem dependencies"
   task :install do
-    $stdout.puts "Use sudo?"
-    use_sudo = $stdin.gets.downcase[0..0] == 'y'
-    $stdout.puts "Generate documentation?"
-    gen_docs = $stdin.gets.downcase[0..0] == 'y'
+    Require::Tasks.set :ask_docs, :ask_sudo
     Require.all(:gem).sort { |a,b| a.name.to_s <=> b.name.to_s }.each do |dsl|
-      cmd = "gem install #{dsl.name} -v '#{dsl.version}'"
-      cmd = "#{'sudo ' if use_sudo}#{cmd} #{'--no-ri --no-rdoc' unless gen_docs}"
-      $stdout.puts cmd
-      system cmd
+      Require::Tasks.run "gem install #{dsl.name} -v '#{dsl.version}'"
     end
   end
 end
