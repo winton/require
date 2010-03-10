63c5aff54deab15cae2dbad4c310bc703abba0c4
winton <mail@wintoni.us>
2010-03-10 15:53:26 -0800

    Version 0.2.5

--- require.rb
+++ require.rb
@@ -11,7 +11,7 @@ Require do
     name 'require'
     homepage "http://github.com/winton/#{name}"
     summary "Manage your project's dependencies with a pretty DSL"
-    version '0.2.4'
+    version '0.2.5'
   end
   
   rakefile do
